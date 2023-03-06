7dad2849b788f52fc4f8697539d76c702839f75e
Sologub Sergey <serjhzz@mail.ru>
2023-03-06 13:57:14 +0300

    new commit

---
+++ views/auth.py
@@ -0,0 +1,25 @@
+from flask_restx import Resource, Namespace
+from flask import request
+
+from implemented import auth_service
+
+
+auth_ns = Namespace('auth')
+
+
+@auth_ns.route('/')
+class AuthView(Resource):
+    def post(self):
+        req_json = request.json
+        username = req_json('username')
+        password = req_json('password')
+        if not all([username, password]):
+            return '', 400
+        tokens = auth_service.generate_tokens(username, password)
+        return tokens, 201
+
+    def put(self):
+        req_json = request.json
+        token = req_json.get('refresh_token')
+        tokens = auth_service.approve_refresh_token(token)
+        return tokens, 201
